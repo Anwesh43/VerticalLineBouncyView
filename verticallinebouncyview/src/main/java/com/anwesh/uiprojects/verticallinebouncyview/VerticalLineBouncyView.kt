d5e91a1c41925082739fd30f9c8a7e2329588f21
Anwesh43 <anweshthecool0@gmail.com>
2019-11-21 10:41:32 +0530

    adding Canvas extension method

--- verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
+++ verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
@@ -10,6 +10,7 @@ import android.app.Activity
 import android.graphics.Paint
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.RectF
 import android.content.Context
 
 val nodes : Int = 5
@@ -27,3 +28,30 @@ fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
 fun Float.cosify() : Float = 1f - Math.cos(Math.PI / 2 + (Math.PI / 2) * this).toFloat()
+
+fun Canvas.drawVerticalLineBouncy(scale : Float, size : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sc : Float = scale.divideScale(1, 2).cosify()
+    for (j in 0..1) {
+        save()
+        scale(1f, 1f - 2 * j)
+        paint.color = lineColor
+        drawLine(0f, h / 2, 0f, h / 2 - (h / 2) * sf, paint)
+        paint.color = rectColor
+        drawRect(RectF(-size, 0f, size, (h / 2) * sc), paint)
+        restore()
+    }
+}
+
+fun Canvas.drawVLBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), h / 2)
+    drawVerticalLineBouncy(scale, size, h, paint)
+    restore()
+}

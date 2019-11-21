4095da72e25e1bd287fbc8f789c42cb53a7b8356
Anwesh43 <anweshthecool0@gmail.com>
2019-11-21 10:31:14 +0530

    adding Int and Float extension methods

--- verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
+++ verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
@@ -21,3 +21,9 @@ val delay : Long = 30
 val lineColor : Int = Color.parseColor("#4CAF50")
 val rectColor : Int = Color.parseColor("#1565C0")
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+fun Float.cosify() : Float = 1f - Math.cos(Math.PI / 2 + (Math.PI / 2) * this).toFloat()

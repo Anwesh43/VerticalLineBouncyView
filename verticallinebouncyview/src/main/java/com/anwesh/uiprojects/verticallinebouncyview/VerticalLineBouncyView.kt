753f1384b5dd8238fd5e3442ffb4b90a0d12ad5d
Anwesh43 <anweshthecool0@gmail.com>
2019-11-21 10:52:03 +0530

    adding VerticalBouncyLine class

--- verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
+++ verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
@@ -138,7 +138,7 @@ class VerticalLineBouncyView(ctx : Context) : View(ctx) {
 
         fun draw(canvas : Canvas, paint : Paint) {
             canvas.drawVLBNode(i, state.scale, paint)
-            next?.draw(canvas, paint)
+            prev?.draw(canvas, paint)
         }
 
         fun update(cb : (Float) -> Unit) {
@@ -159,6 +159,29 @@ class VerticalLineBouncyView(ctx : Context) : View(ctx) {
             }
             cb()
             return this
+        }
+    }
+
+    data class VerticalBouncyLine(var i : Int) {
+
+        private var curr : VLBNode = VLBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }

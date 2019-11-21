e41394a95894eb8ebee93a15fd5c09745a7ccd1e
Anwesh43 <anweshthecool0@gmail.com>
2019-11-21 10:49:51 +0530

    adding VLBNode class

--- verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
+++ verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
@@ -119,4 +119,46 @@ class VerticalLineBouncyView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VLBNode(var i : Int, val state : State = State()) {
+
+        private var next : VLBNode? = null
+        private var prev : VLBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = VLBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVLBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VLBNode {
+            var curr : VLBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }

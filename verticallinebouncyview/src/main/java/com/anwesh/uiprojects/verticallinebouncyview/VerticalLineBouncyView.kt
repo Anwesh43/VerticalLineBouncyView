d4db56b9e3e216109769a95c3fcc274490ba3b25
Anwesh43 <anweshthecool0@gmail.com>
2019-11-21 10:28:23 +0530

    adding constants and libraires

--- verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
+++ verticallinebouncyview/src/main/java/com/anwesh/uiprojects/verticallinebouncyview/VerticalLineBouncyView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.verticallinebouncyview
 /**
  * Created by anweshmishra on 21/11/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 5
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.02f
+val delay : Long = 30
+val lineColor : Int = Color.parseColor("#4CAF50")
+val rectColor : Int = Color.parseColor("#1565C0")
+val backColor : Int = Color.parseColor("#BDBDBD")
